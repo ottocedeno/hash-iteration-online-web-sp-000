188201530349c91505795f255c0b9946d3251a62
Otto Cedeno <otto@ottostacos.com>
2019-11-21 18:25:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,5 +12,8 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-
+  birthday_kids.each do |kids_name, age|
+    if age > 12
+      puts "You are too old for this."
+    end
 end
